fe53de16e2d83526b151bd4e623b0a3d3aa9765b
V Chandrasekhar Valiveti <v.chandrasekhar.valiveti@ibm.com>
2020-10-14 20:46:33 +0530

    adding provider, backend and repo .tf files

---
+++ backend.tf
@@ -0,0 +1,11 @@
+# Add the terraform cloud backend for running locally
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "terraform-cloud-iac-github"
+
+    workspaces {
+      name = "github-management"
+    }
+  }
+}
---
+++ respository.tf
@@ -0,0 +1,15 @@
+# The terraform resource for the repository
+resource "github_repository" "github-management" {
+  name        = "github-management"
+  description = "Terraform based repository to manage all our GutHub repositories"
+
+  private            = true
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  auto_init          = false
+  license_template   = "mit"
+  topics             = ["config", "terraform"]
+}
